21aa83d195277ebae26e1fd3114c76584027bf8b
ToymanShikhar <shikharverma043@gmail.com>
2021-03-11 15:20:28 +0530

    Fix bug

--- index.js
+++ index.js
@@ -31,12 +31,9 @@ const saltRounds = 10;
 
 const id = process.env.ID;
 const password = process.env.PASSWORD;
-const database_url =
-    "mongodb+srv://" +
-    "shikhar" +
-    ":" +
-    "SVERMA@123" +
-    "@cluster0.fmnu9.mongodb.net/covidDB";
+const database_url = "mongodb+srv://admin-shikhar:SVERMA@123@cluster0.fmnu9.mongodb.net/covidDB";
+
+// const database_url = "mongodb://localhost:27017/covidDB";
 
 mongoose.connect(database_url, {
     useNewUrlParser: true,
